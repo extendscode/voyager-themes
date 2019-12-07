89427a9cb016787c7ce53eaf31406db4ac755317
David Turnes <d3turnes@gmail.com>
2019-12-07 14:07:12 +0100

    do compatible version >= 1.2.6

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,32 @@
 <?php
 
+if (!class_exists(RowTypeClass)) {
+	class RowTypeClass {
+				
+		public $required;
+		public $field;
+		public $type;
+		public $details;
+		public $placeholder;
+		
+		public function __construct($args) { foreach ($args as $key => $value) { $this->{$key} = $value; } }
+		
+		// remove getTranslatedAttribute placeholder input text
+		public function getTranslatedAttribute($attribute) { return $attribute; }
+	};
+}
+
+if (!class_exists(DataTypeContentClass)) {
+	class DataTypeContentClass {
+		
+		public $key;
+		public $content;
+		
+		public function __construct($args) { foreach ($args as $key => $value) { $this->{$key} = $value; } }
+		
+		public function getKey() { return $this->key; }
+	}
+}
 
 if (!function_exists(theme_field)) {
 
@@ -10,13 +37,23 @@ if (!function_exists(theme_field)) {
 
         $option_exists = $theme->options->where('key', '=', $key)->first();
 
+        $id = 0;
+		$content = "";
         if (isset($option_exists->value)) {
-            $content = $option_exists->value;
+            $id = $option_exists->id;
+			$content = $option_exists->value;
         }
-
-        $row = (object)['required' => $required, 'field' => $key, 'type' => $type, 'details' => $details, 'display_name' => $placeholder];
-        $dataTypeContent = (object)["id" => 0, $key => $content];
-        //$dataTypeContent = (object)[$key => $content];
+		
+		if (version_compare(substr(app('voyager')->getVersion(),1), '1.2.6', '<=')):
+			$row = (object)['required' => $required, 'field' => $key, 'type' => $type, 'details' => $details, 'display_name' => $placeholder];
+			$dataTypeContent = (object)["id" => $id, $key => $content];
+			//$dataTypeContent = (object)[$key => $content];
+        else:
+			// remove getTranslatedAttribute()
+			$row = new RowTypeClass(['required' => $required, 'field' => $key, 'type' => $type, 'details' => $details, 'display_name' => $placeholder]);
+			$dataTypeContent = new DataTypeContentClass(['id' => $id, $key => $content]);
+		endif;
+		
         $label = '<label for="' . $key . '">' . $title . '<span class="how_to">You can reference this value with <code>theme(\'' . $key . '\')</code></span></label>';
         $details = '<input type="hidden" value="' . $details . '" name="' . $key . '_details__theme_field">';
         $type = '<input type="hidden" value="' . $type . '" name="' . $key . '_type__theme_field">';
